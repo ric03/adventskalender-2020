2b1a8a15f8b5224e9b93ff88e8804281485ba525
Tom <52013041+ric03@users.noreply.github.com>
2020-11-25 00:17:39 +0100

    feat: add substitution-cipher

---
+++ src/util/substitution-cipher.js
@@ -0,0 +1,27 @@
+export function substitutionCipher(text, dictionary) {
+    if (!text || text.length === 0) return '';
+    if (!isDictionaryValid(dictionary)) return text;
+
+    const DICT = dictionary.toLowerCase().split('');
+
+    return text.split('')
+        .map(char => substituteSingleChar(char, DICT))
+        .reduce(toString);
+}
+
+function isDictionaryValid(dictionary) {
+    return dictionary
+        && dictionary.length === 26 // a-z
+        && new Set(dictionary.split('')).size === 26; // unique entries
+}
+
+function substituteSingleChar(char, DICT) {
+    const charCode = char.toLowerCase().charCodeAt(0);
+    const a = 'a'.charCodeAt(0);
+    const z = 'z'.charCodeAt(0);
+    if (charCode < a || z < charCode) return char;
+    const idx = charCode - a;
+    return DICT[idx];
+}
+
+const toString = (acc, cur) => acc + cur;
---
+++ src/util/substitution-cipher.spec.js
@@ -0,0 +1,43 @@
+import {substitutionCipher} from "./substitution-cipher";
+
+describe('substitution-cipher', function () {
+    it('should return empty string on null', function () {
+        expect(substitutionCipher(null, null)).toEqual('');
+    });
+
+    it('should return the passed text on dictionary being null', function () {
+        expect(substitutionCipher('Hello', null)).toEqual('Hello');
+    });
+
+    it('should return the passed text on dictionary being too short', function () {
+        expect(substitutionCipher('Hello', '1234567890123456789012345')).toEqual('Hello');
+    });
+
+    it('should return the passed text on the dictionary being too long', function () {
+        expect(substitutionCipher('Hello', '123456789012345678901234567')).toEqual('Hello');
+    });
+
+    it('should return the passed text on the dictionary containing non unique characters', function () {
+        expect(substitutionCipher('Hello', '12345678901234567890123456')).toEqual('Hello');
+    });
+
+    it('should encrypt a single char', function () {
+        const dict = 'kbcdefghijalmnopqrstuvwxyz';
+        expect(substitutionCipher('a', dict)).toEqual('k');
+    });
+
+    it('should encrypt multiple chars only [a-z]', function () {
+        const dict = 'qrstuvwxyzkbcdefghijalmnop';
+        expect(substitutionCipher('mytinystring', dict)).toEqual('cojydoijhydw');
+    });
+
+    it('should encrypt multiple chars only [a-z]', function () {
+        const dict = 'qrstuvwxyzkbcdefghijalmnop';
+        expect(substitutionCipher('MY TINY STRING', dict)).toEqual('co jydo ijhydw');
+    });
+
+    it('should encrypt multiple chars with special characters', function () {
+        const dict = 'qrstuvwxyzkbcdefghijalmnop';
+        expect(substitutionCipher('my tiny string !@#', dict)).toEqual('co jydo ijhydw !@#');
+    });
+})
