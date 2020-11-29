c53f12e08a754493a16ba8c73bf152e068ca08dc
Tom <52013041+ric03@users.noreply.github.com>
2020-11-29 23:47:12 +0100

    feat: improve content - add better text and images, - change tile order

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import {asciiToBinary} from "./util/ascii-to-binary";
+import {caesarCipher} from "./util/caesar-cipher";
+import {piCipher} from "./util/pi-cipher";
 import {shuffleArray} from "./util/shuffle-array";
+import {substitutionCipherWrapper} from "./util/substitution-cipher";
 
 window.DISABLE_DATE_LOCK = false;
 
@@ -58,9 +62,9 @@ function canOpenTile(id) {
     return isDecember && isPreviousOrSameDay;
 }
 
-function fillPopupWithContent({id, title, msg, img}) {
+function fillPopupWithContent({id, title, msg, textTransform, img}) {
     document.getElementById('popup-title').innerText = `#${id} ${title}`;
-    document.getElementById('popup-message').innerText = msg;
+    document.getElementById('popup-message').innerText = textTransform ? textTransform(msg) : msg;
 
     const image = document.getElementById('popup-image');
     image.src = 'img/' + img.src;
@@ -74,65 +78,70 @@ const hideForbidden = () => document.getElementById('forbidden').style.display =
 
 const DAYS = [
     {
-        id: 1, title: 'Eine erste kleine Überraschung', msg: 'Alle Jahre wieder :D',
-        img: {alt: 'Adventskalender', src: 'david-olivier-gascon-e9vrm-J192k-unsplash.jpg'}
+        id: 1, title: '',
+        msg: 'Beinahe hätte es ein Türchen für jeden Ehemaligen geben können. AA B DD E F II JJ K M NN SS TT.',
+        img: {alt: 'Team', src: 'team-cartoon.png'}
     }, {
-        id: 2, title: 'Hi Peter', msg: 'Der zweite Streich folgt zugleich',
-        img: {alt: '', src: ''}
-    }, {
-        id: 3, title: '', msg: `catch(error) { window.open(\`https://www.google.com/search?q=$\{error}\`)}`,
-        img: {alt: 'VW Beetle', src: 'giovanni-ribeiro-B1hX9TE7HDY-unsplash.jpg'}
-    }, {
-        id: 4, title: '', msg: 'while(true) neverStop()',
-        img: {alt: 'Stop sign', src: 'free-to-use-sounds-Vkt3uDeDkdg-unsplash.jpg'}
-    }, {
-        id: 5, title: '', msg: 'Keep on going.',
+        id: 2, title: '', msg: 'Keep on going.',
         img: {alt: 'Toilet paper countdown', src: 'mick-haupt-S5Lz8y95m1A-unsplash.jpg'}
     }, {
-        id: 6, title: '', msg: 'Einfach laufen, laufen, laufen ...',
-        img: {alt: 'Walking Man', src: 'harsh-gupta-nVDB1IGq64s-unsplash.jpg'}
+        id: 3, title: '',
+        msg: 'Connected everything is. The clues you must find. May the force be with you.',
+        textTransform: asciiToBinary,
+        img: {alt: ' ', src: ''}
     }, {
-        id: 7, title: '', msg: 'Today there is just this beautiful image',
-        img: {alt: 'abstract image', src: 'pawel-czerwinski-6lQDFGOB1iw-unsplash.jpg'}
+        id: 4, title: '', msg: 'Es gibt jetzt zwei Tobias Huber.',
+        img: {alt: 'Macbook with coffee mug on table', src: 'chris-montgomery-smgTvepind4-unsplash.jpg'}
     }, {
-        id: 8, title: '', msg: 'Mit Ruhe und Gemutlichkeit',
-        img: {alt: 'Snowy tree', src: 'mike-blank-L23AkTe3y20-unsplash.jpg'}
+        id: 5, title: '', msg: `catch(error) { window.open(\`https://www.google.com/search?q=$\{error}\`)}`,
+        img: {alt: 'VW Beetle', src: 'giovanni-ribeiro-B1hX9TE7HDY-unsplash.jpg'}
     }, {
-        id: 9, title: '', msg: 'Einen Schritt nach dem anderen machen',
-        img: {alt: '', src: ''}
-    }, {
-        id: 10, title: '', msg: 'Holla die Waldfee',
-        img: {alt: '', src: ''}
-    }, {
-        id: 11, title: '', msg: 'WTF - Wespe Token Factory',
-        img: {alt: 'Yoda Oh', src: 'reno-laithienne-aN3PdTgW3tc-unsplash.jpg'}
-    }, {
-        id: 12, title: '', msg: 'Never ever stop believing',
-        img: {alt: 'Lady driving', src: 'spencer-davis-hi1Iq4x_ldM-unsplash.jpg'}
-    }, {
-        id: 13, title: '', msg: 'Dont forget to drink',
-        img: {alt: 'Beer cheers', src: 'wil-stewart-UErWoQEoMrc-unsplash.jpg'}
-    }, {
-        id: 14, title: 'Cheers', msg: 'Dont drink and drive. Exception: functional alcoholics',
-        img: {alt: 'Lady in trunk', src: 'sule-makaroglu-BTOPcWXBCGg-unsplash.jpg'}
-    }, {
-        id: 15, title: '', msg: 'Lets get ready. Oh stil 9 days remaining',
-        img: {alt: 'Blue Parrot', src: 'jose-ignacio-garcia-zajaczkowski-ZkBjtrG7aYg-unsplash.jpg'}
-    }, {
-        id: 16, title: '', msg: 'Did you get your presents yet?',
-        img: {alt: 'Present', src: 'annie-spratt-HZuJfZlXVQY-unsplash.jpg'}
-    }, {
-        id: 17, title: '', msg: 'Viele Wege führen zum Ziel, meiner ist es oft nicht. - Emil',
+        id: 6, title: '', msg: 'Viele Wege führen nach Rom, meiner ist es oft nicht.',
         img: {alt: 'Colosseum', src: 'l-a-l-a-s-z-a-S0hS0HfH_B8-unsplash.jpg'}
     }, {
-        id: 18, title: '', msg: 'Hows your day?',
+        id: 7, title: '', msg: 'lftqskgixhnwo', textTransform: caesarCipher,
+        img: {alt: 'Salad', src: 'raphael-nogueira-63mHpYEyjCA-unsplash.jpg'}
+    }, {
+        id: 8, title: '', msg: 'WTF - Wespe Token Factory',
+        img: {alt: 'Yoda Oh', src: 'reno-laithienne-aN3PdTgW3tc-unsplash.jpg'}
+    }, {
+        id: 9, title: '', msg: 'Fast fertig...',
+        img: {alt: 'Piggy bank', src: 'damir-spanic-vwaTtIhCjVg-unsplash.jpg'}
+    }, {
+        id: 10, title: '', msg: 'evybpzcdjmuar', textTransform: piCipher,
+        img: {alt: 'Apple pie', src: 'hugo-aitken-PqYvDBwpXpU-unsplash.jpg'}
+    }, {
+        id: 11, title: '', msg: 'Ein Cookie (* keine Einwilligung erforderlich)',
+        img: {alt: 'Delicious Cookie', src: 'amirali-mirhashemian-x6VuEH2npHw-unsplash.jpg'}
+    }, {
+        id: 12, title: '', msg: 'Today there is just this beautiful image',
+        img: {alt: 'abstract image', src: 'pawel-czerwinski-6lQDFGOB1iw-unsplash.jpg'}
+    }, {
+        id: 13, title: '', msg: 'Did you get your presents yet?',
+        img: {alt: 'Present', src: 'annie-spratt-HZuJfZlXVQY-unsplash.jpg'}
+    }, {
+        id: 14, title: '',
+        msg: 'Gratuliere du hast es bis zum Ende geschafft. Auch wenn hier kein Gold gefuellter Topf steht, hoffe ich dennoch, dass es dir Spass gemacht hat.',
+        textTransform: substitutionCipherWrapper('lftqskgixhnwoevybpzcdjmuar'),
+        img: {alt: '', src: ''}
+    }, {
+        id: 15, title: '', msg: '',
+        img: {alt: 'Comic', src: 'Strip-Plus-qua-coder-la-feature-650-finalenglish.jpg'}
+    }, {
+        id: 16, title: '', msg: 'Mit Ruhe und Gemutlichkeit',
+        img: {alt: 'Snowy tree', src: 'mike-blank-L23AkTe3y20-unsplash.jpg'}
+    }, {
+        id: 17, title: '', msg: 'Hows your day?',
         img: {alt: 'tropical beach', src: 'nattu-adnan-vvHRdOwqHcg-unsplash.jpg'}
     }, {
-        id: 19, title: '', msg: 'Build failed.',
-        img: {alt: 'Sparkler', src: 'wout-vanacker-l4HBYkURqvE-unsplash.jpg'}
+        id: 18, title: '', msg: '',
+        img: {alt: 'Miniature scene: family with christmas tree', src: 'tyler-delgado-Rb0ZQ8v1siY-unsplash.jpg'}
     }, {
-        id: 20, title: '', msg: 'Are you hungry?',
-        img: {alt: 'Breakfast', src: 'joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg'}
+        id: 19, title: '', msg: 'Lets get ready. Only 5 days remaining',
+        img: {alt: 'Blue Parrot', src: 'jose-ignacio-garcia-zajaczkowski-ZkBjtrG7aYg-unsplash.jpg'}
+    }, {
+        id: 20, title: 'Cheers', msg: 'Dont drink and drive. Exception: functional alcoholics',
+        img: {alt: 'Lady in trunk', src: 'sule-makaroglu-BTOPcWXBCGg-unsplash.jpg'}
     }, {
         id: 21, title: 'Fly like a eagle', msg: 'Stand up, close your eyes and start running',
         img: {alt: '', src: 'valdemaras-d-Nx2FTJfKX68-unsplash.jpg'}
